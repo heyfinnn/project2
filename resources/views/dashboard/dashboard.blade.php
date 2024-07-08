2239b3568d9f2869cee9c4b9a4291456eb3fcc4c
heyfinnn <radhityamalikuss01@gmail.com>
2024-07-08 13:29:35 +0700

    p

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -16,7 +16,7 @@
         @else
             <div class="alert alert-primary alert-dismissible fade show">
                 <svg viewbox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><circle cx="12" cy="12" r="10"></circle><path d="M8 14s1.5 2 4 2 4-2 4-2"></path><line x1="9" y1="9" x2="9.01" y2="9"></line><line x1="15" y1="9" x2="15.01" y2="9"></line></svg>
-                <strong>Hi {{ auth()->user()->name }}, </strong>You are logged in!
+                <strong>Hi {{ auth()->user()->name }}, </strong>kamu berhasil login!
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
                 </button>
             </div>    
--- resources/views/dashboard/profile/profile.blade.php
+++ resources/views/dashboard/profile/profile.blade.php
@@ -16,7 +16,7 @@
         @else
             <div class="alert alert-primary alert-dismissible fade show">
                 <svg viewbox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><circle cx="12" cy="12" r="10"></circle><path d="M8 14s1.5 2 4 2 4-2 4-2"></path><line x1="9" y1="9" x2="9.01" y2="9"></line><line x1="15" y1="9" x2="15.01" y2="9"></line></svg>
-                <strong>Hi {{ auth()->user()->name }}, </strong>You are logged in!
+                <strong>Hi {{ auth()->user()->name }}, </strong>kamu berhasil login!
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
                 </button>
             </div>    
@@ -28,7 +28,7 @@
                     <div class="col-xl-12">
                     <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Input Style</h4>
+                                <h4 class="card-title">UPDATE PASSWORD</h4>
                             </div>
                             <div class="card-body">
                                 <div class="basic-form">
